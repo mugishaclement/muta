fd2298fca02a4ea3fa850b71ffc32e8029d6fafd
mugishaclement <mugishaclement250@gmail.com>
2025-01-30 09:25:17 +0200

    mugih

---
+++ android/app/src/main/kotlin/com/example/muta/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.muta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  
+runApp(MaterialApp(
+  home:Center(
+    child:Text('Mugisha clement',style: TextStyle(color: const Color.fromARGB(255, 179, 186, 192)),),
+  )
+  
+));
+}
